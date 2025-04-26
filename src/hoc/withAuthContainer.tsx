02351761abc3c4f553dd77a69f3cae0213182d70
AbdullahEltony <abdullahmeltony@gmail.com>
2025-04-27 01:20:33 +0300

    Modified with auth container to resolve the vercle issue

--- src/hoc/withAuthContainer.tsx
+++ src/hoc/withAuthContainer.tsx
@@ -2,11 +2,8 @@
 
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
-import Cookies from "js-cookie";
 
-export default function withAuth<T extends object>(
-  Component: React.ComponentType<T>
-) {
+export default function withAuth<T extends object>(Component: React.ComponentType<T>) {
   return function AuthComponent(props: T) {
     const router = useRouter();
 
@@ -20,15 +17,13 @@ export default function withAuth<T extends object>(
       const confirmEmail = async () => {
         if (userId && code) {
           try {
+            const Cookies = (await import("js-cookie")).default;
             Cookies.set("token", code, { expires: 7, path: "/" }); // Store token
-             await fetch("/api/auth/confirm-email",
-              {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({ userId, code }),
-              }
-            );
-
+            await fetch("/api/auth/confirm-email", {
+              method: "POST",
+              headers: { "Content-Type": "application/json" },
+              body: JSON.stringify({ userId, code }),
+            });
           } catch (error) {
             console.error("Fetch error:", error);
           }
@@ -39,12 +34,16 @@ export default function withAuth<T extends object>(
     }, [router]);
 
     useEffect(() => {
-      const token = Cookies.get("token");
-      if (!token) {
-        router.replace("/auth/login");
-      } 
-    }, [router]);
+      const checkAuth = async () => {
+        const Cookies = (await import("js-cookie")).default;
+        const token = Cookies.get("token");
+        if (!token) {
+          router.replace("/auth/login");
+        }
+      };
 
+      checkAuth();
+    }, [router]);
 
     return <Component {...props} />;
   };
