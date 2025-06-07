8fbc7a51c798b15ba0b9cffdd2a978ff27e9719a
Hazem Helal <hazemhelal.se@gmail.com>
2025-06-08 02:55:37 +0300

    Refactor Home component layout for improved spacing and responsiveness

--- src/app/(pages)/home/page.tsx
+++ src/app/(pages)/home/page.tsx
@@ -8,21 +8,20 @@ import "react-toastify/dist/ReactToastify.css";
 import "swiper/css";
 
 export default function Home() {
-  return (
-    <div className="overflow-hidden mb-[70px]">
-      <section id="home">
+  return (    <div className="overflow-hidden mb-[70px]">
+      <section id="home" className="sm:mb-64 lg:mb-64">
         <HomeSlider />
       </section>      <div className="container mx-auto px-4 sm:px-6 md:px-8">
-        <section className="mt-48 sm:mt-56 lg:mt-64">
+        <section className=" sm:mt-24 mb-[4rem] md:mb-0">
           <Top10 />
         </section>
-        <section className="mt-24 sm:mt-32 mb-[4rem] md:mb-0">
+        <section className=" sm:mt-24 mb-[4rem] md:mb-0">
           <TopRated />
         </section>
-        <section className="mt-24 sm:mt-32 mb-[4rem] md:mb-0">
+        <section className=" sm:mt-24 mb-[4rem] md:mb-0">
           <Recommended />
         </section>
-        <section className="mt-24 sm:mt-32 mb-[4rem] md:mb-0">
+        <section className=" sm:mt-24 mb-[4rem] md:mb-0">
           <Geners />
         </section>
       </div>
