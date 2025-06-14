ca23254a8f096f20ca0961eb7a3972abce28d0a6
AbdullahEltony <abdullahmeltony@gmail.com>
2025-06-14 14:29:48 +0300

    test

--- src/app/(pages)/movies/[movieId]/page.tsx
+++ src/app/(pages)/movies/[movieId]/page.tsx
@@ -5,7 +5,7 @@ import Actors from "@/components/movies/Actors";
 import Reviews from "@/components/movies/Reviews";
 import SimilarMovies from "@/components/movies/SimilarMovies";
 import "react-toastify/dist/ReactToastify.css";
-import MovieInfo from "@/components/movies/MovieInfo";
+// import MovieInfo from "@/components/movies/MovieInfo";
 import { ToastContainer } from "react-toastify";
 import { usePathname } from "next/navigation";
 import useFetch from "@/hooks/useFetch";
@@ -21,33 +21,33 @@ const MovieDetails = () => {
   const pathname = usePathname();
   const tmdbid = pathname.split("/")[2];
   const { data,loading } = useFetch<MovieDetails>(`/api/Movie/` + tmdbid);
-  const info = {
-    tmdbId: data?.tmdbId,
-    title: data?.title,
-    tagline: data?.tagline,
-    releaseDate: data?.releaseDate,
-    posterPath: data?.posterPath,
-    logoPath: data?.logoPath,
-    imdbRating: data?.imdbRating,
-    rottenTomatoesRating: data?.rottenTomatoesRating,
-    metacriticRating: data?.metacriticRating,
-    mpa: data?.mpa,
-    backdropPath: data?.backdropPath,
-    runtime: data?.runtime,
-    overview: data?.overview,
-    geners: data?.genresDetails,
-    movieReviews: data?.movieReviews,
-    stars: data?.stars,
-    isLiked: data?.isLiked,
-    isWatched: data?.isWatched,
-    isInWatchList: data?.isInWatchList,
-    trailer: data?.trailer,
-    streamingLink: data?.tmdbId.toString(),
-  };
+  // const info = {
+  //   tmdbId: data?.tmdbId,
+  //   title: data?.title,
+  //   tagline: data?.tagline,
+  //   releaseDate: data?.releaseDate,
+  //   posterPath: data?.posterPath,
+  //   logoPath: data?.logoPath,
+  //   imdbRating: data?.imdbRating,
+  //   rottenTomatoesRating: data?.rottenTomatoesRating,
+  //   metacriticRating: data?.metacriticRating,
+  //   mpa: data?.mpa,
+  //   backdropPath: data?.backdropPath,
+  //   runtime: data?.runtime,
+  //   overview: data?.overview,
+  //   geners: data?.genresDetails,
+  //   movieReviews: data?.movieReviews,
+  //   stars: data?.stars,
+  //   isLiked: data?.isLiked,
+  //   isWatched: data?.isWatched,
+  //   isInWatchList: data?.isInWatchList,
+  //   trailer: data?.trailer,
+  //   streamingLink: data?.tmdbId.toString(),
+  // };
   return (
     <div className="min-h-screen bg-mainBg">
       {/* Movie Info - Full width background section */}
-      <MovieInfo info={info!} loading={loading}/>
+      {/* <MovieInfo info={info!} loading={loading}/> */}
       
       {/* Container for all other sections with proper margins and padding */}
       <div className="container mx-auto px-4 sm:px-6 md:px-8 pt-8 pb-20">
